ebcadc14bd0b5de157ff3c68fc5bb9b0bf6e8795
Vincent Darmawan <vincentd12346@gmail.com>
2021-10-08 10:32:54 +0700

    setup

---
+++ app.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    print("App Started")
+
+    t = np.arange(0., 5., 0.2)
+    plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+    plt.show()
